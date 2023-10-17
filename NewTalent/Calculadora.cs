282f5b20cc14246b09ed8e6a499f3f9090bee6d3
Moises Lima <moises.pl@hotmail.com>
2023-10-17 20:40:52 -0300

    first commit

---
+++ NewTalent/Calculadora.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewTalent
+{
+    public class Calculadora
+    {
+        private List<string> listaHistorico;
+        private string data;
+
+        public Calculadora(string data)
+        {
+            listaHistorico = new List<string>();
+            this.data = data;
+        }
+
+        public int Somar(int val1, int val2)
+        {
+            int resultado = val1 + val2;
+            listaHistorico.Insert(0, "Res: " + resultado + " - data: " + data);
+            return resultado;
+        }
+        public int Subtrair(int val1, int val2)
+        {
+            int resultado = val1 - val2;
+            listaHistorico.Insert(0, "Res: " + resultado + " - data: " + data);
+            return resultado;
+        }
+        public int Multiplicar(int val1, int val2)
+        {
+            int resultado = val1 * val2;
+            listaHistorico.Insert(0, "Res: " + resultado + " - data: " + data);
+            return resultado;
+        }
+        public int Dividir(int val1, int val2)
+        {  
+                int resultado = val1 / val2;
+                listaHistorico.Insert(0, "Res: " + resultado + " - data: " + data);
+                return resultado;
+        }
+        public List<string> Historico()
+        {
+            listaHistorico.RemoveRange(3, listaHistorico.Count - 3);
+            return listaHistorico;
+        }
+    }
+}
